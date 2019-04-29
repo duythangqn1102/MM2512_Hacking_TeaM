db04306e776061cdde249e8e50d065851a309762
Trương Minh Quý <truongminhquy46@gmail.com>
2019-04-29 22:44:39 +0700

    Add files via upload

---
+++ ScriptOnline/4T_Rank_Kin_ᴅᴇᴄ_ʙʏ_ᴍᴍ2512.lua
@@ -0,0 +1,214 @@
+function Main()
+  SN = gg.multiChoice({
+ "☯Speed New +20KM[ 1 Lần Huấn Luyện]",
+ "☯Speed New +50KM[ 1 Lần Huấn Luyện]",
+"☯ CWW Nhanh [ɢᴀᴍᴇ]",
+"☯ ɴɪᴛʀᴏ 30%[ɢᴀᴍᴇ]",
+"☯ ɴɪᴛʀᴏ 70%[ɢᴀᴍᴇ]",
+"☯ ɴɪᴛʀᴏ 100%[ɢᴀᴍᴇ]",
+"☯ Khóa Phanh[1 Lần]",
+"----->⚠️EXIT⚠️<-----",
+ }, nil,'⚡4𝕿 𝕲𝖆𝖒𝖎𝖓𝖌⚡ \n ▪️ᶻᵃˡᵒ: 0826526230:\n ▪️ᵀᵃ̀ᶦ ᴷʰᵒᵃ̉ⁿ :ProPlayer 4T\n 🕘ᴴᴱ̂́ᵀ ᴴᴬ̣ᴺ:Mùa 4')
+  if SN == nil then
+  else
+  if SN[1] == true then a1() end
+  if SN[2] == true then a2() end
+  if SN[3] == true then a3() end
+  if SN[4] == true then a4() end
+  if SN[5] == true then a5() end
+  if SN[6] == true then a6() end
+  if SN[7] == true then a7() end
+  if SN[8] == true then  Exit() end
+end
+  XGCK = -1
+end
+
+
+
+
+function a1()
+gg.clearResults()
+gg.setRanges (4)
+gg.searchNumber('0.27777779102F;1000.0F;0.0001F;-1.0F;1.0F', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.27777779102', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2, 2)
+gg.editAll("0.34", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber('0.27777779102F;1000.0F;0.0001F;-1.0F;1.0F', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.27777779102', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2, 2)
+gg.editAll("0.38", gg.TYPE_FLOAT)
+gg.clearResults()
+end
+
+function a2()
+gg.clearResults()
+gg.setRanges (4)
+gg.searchNumber('0.27777779102F;1000.0F;0.0001F;-1.0F;1.0F', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.27777779102', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2, 2)
+gg.editAll("0.4", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber('0.27777779102F;1000.0F;0.0001F;-1.0F;1.0F', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.27777779102', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2, 2)
+gg.editAll("0.38", gg.TYPE_FLOAT)
+gg.clearResults()
+end
+
+function a3()
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("35;0.5;0.6;1.399;50.00000381472", 16, false, 536870912, 0, -1)
+gg.searchNumber("35", 16, false, 536870912, 0, -1)
+gg.getResults(1000)
+gg.editAll("0", 16)
+gg.clearResults()
+end
+
+function a4()
+gg.setRanges(32)
+gg.searchNumber("45F;1D;0;4.40000009537::150", 16, false, 536870912, 0, -1)
+gg.searchNumber("0", 16, false, 536870912, 0, -1)
+gg.getResults(1000)
+gg.editAll("9", 16)
+gg.clearResults()
+end
+
+function a5()
+gg.setRanges(32)
+gg.searchNumber("45F;1D;0;4.40000009537::150", 16, false, 536870912, 0, -1)
+gg.searchNumber("0", 16, false, 536870912, 0, -1)
+gg.getResults(1000)
+gg.editAll("15", 16)
+gg.clearResults()
+end
+
+function a6()
+gg.setRanges(32)
+gg.searchNumber("45F;1D;0;4.40000009537::150", 16, false, 536870912, 0, -1)
+gg.searchNumber("0", 16, false, 536870912, 0, -1)
+gg.getResults(1000)
+gg.editAll("9999", 16)
+gg.clearResults()
+end
+
+
+function a7()
+gg.clearResults()
+gg.setRanges(4)
+gg.searchNumber("1;-0.10000000149;1000::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1000", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.clearResults()
+end
+
+function a12()
+  A11 = gg.multiChoice({
+"🔎ɢᴏ́ᴄ ɴʜɪ̀ɴ ɢᴀ̂̀ɴ",
+  "🔎ɢᴏ́ᴄ ɴʜɪ̀ɴ x5",
+  "🔎ɢᴏ́ᴄ ɴʜɪ̀ɴ x10",
+"🔎ɢᴏ́ᴄ ɴʜɪ̀ɴ ᴋɪᴇ̂̉ᴜ 1",
+  "🔎ɢᴏ́ᴄ ɴʜɪ̀ɴ ᴋɪᴇ̂̉ᴜ 2",
+  "🔎ɢᴏ́ᴄ ɴʜɪ̀ɴ ᴋɪᴇ̂̉ᴜ 3",
+  "➡️ᴠᴇ̂̀ ᴍᴇɴᴜ"
+ }, nil, "⚠️ǫᴜᴀʏ ᴄʟɪᴘ ʜɪɢʜᴛʟɪɢʜᴛ ᴄᴜ̛̣ᴄ ᴄʜᴀ̂́ᴛ⚠️")
+  if SN == nil then
+  else
+  if A11[1] == true then
+   z1()
+  end
+  if A11[2] == true then
+   z2()
+  end
+  if A11[3] == true then
+   z2z()
+  end
+  if A11[4] == true then
+   z3()
+  end
+  if A11[5] == true then
+   z4()
+  end
+  if A11[6] == true then
+   z5()
+  end
+  if A11[7] == true then
+   Main()
+  end
+end
+  XGCK = -1
+end
+
+function z1()
+gg.clearResults()
+	 gg.setRanges(32)
+	 gg.searchNumber("4.09999990463", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+	 gg.getResults(10)
+	 gg.editAll("1", gg.TYPE_FLOAT)
+	 gg.clearResults()
+end
+
+function z2()
+gg.clearResults()
+	 gg.setRanges(32)
+	 gg.searchNumber("4.09999990463", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+	 gg.getResults(10)
+	 gg.editAll("12", gg.TYPE_FLOAT)
+	 gg.clearResults()
+end
+
+function z2z()
+gg.clearResults()
+	 gg.setRanges(32)
+	 gg.searchNumber("4.09999990463", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+	 gg.getResults(10)
+	 gg.editAll("20", gg.TYPE_FLOAT)
+	 gg.clearResults()
+end
+
+function z3()
+gg.clearResults()
+	 gg.setRanges(32)
+	 gg.searchNumber("7.19999980927", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+	 gg.getResults(100)
+	 gg.editAll("999", gg.TYPE_FLOAT)
+	 gg.clearResults()
+end
+
+function z4()
+gg.clearResults()
+	 gg.setRanges(32)
+	 gg.searchNumber("1.79999995232", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+	 gg.getResults(100)
+	 gg.editAll("-5", gg.TYPE_FLOAT)
+	 gg.clearResults()
+end
+
+function z5()
+gg.clearResults()
+	 gg.setRanges(32)
+	 gg.searchNumber("1.79999995232", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+	 gg.getResults(100)
+	 gg.editAll("-5", gg.TYPE_FLOAT)
+	gg.clearResults()
+end
+
+function Exit()
+print("4T DZ")
+os.exit()
+gg.setVisible(true)
+end
+
+
+while true do
+  if gg.isVisible(true) then
+    XGCK = 1
+    gg.setVisible(false)
+  end
+  gg.clearResults()
+  if XGCK == 1 then
+    Main()
+  end
+end
